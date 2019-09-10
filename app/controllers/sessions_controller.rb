433a8a1b86d8b6486e6bcc86989f3e49e003076d
github username <aude53@gmail.com>
2019-09-10 23:06:29 +0100

    reduce login fields size and centered library title

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,22 +6,17 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by(email_address: params[:email_address])
 
-        # if !params[:email_address].present? || !params[:password].present? || !@user.authenticate(params[:password])
-        #     redirect_to login_path
-        # else
-        # byebug
         if @user == nil
             redirect_to login_path
-            flash[:errors] = ["Not logged in!"]
-            # return head(:forbidden)
-            # unless @user.authenticate(params[:password])
+            flash[:errors] = ["No logged in!"]
         else 
             if @user.authenticate(params[:password])
-            session[:user_id] = @user.id
-            flash[:notices] = ["Logged in!"]
-            redirect_to root_path
-            else redirect_to login_path
-            flash[:errors] = ["Not logged in!"]
+                session[:user_id] = @user.id
+                flash[:notices] = ["Logged in!"]
+                redirect_to root_path
+            else 
+                flash[:errors] = ["Not logged in!"]
+                redirect_to login_path
             end
         end
     end
